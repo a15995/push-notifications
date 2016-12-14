3c23c50a6ba65efa4d5e124c0b1dc9d64fa3018b
a15995 <123@123.dk>
2016-12-14 15:33:11 +0100

    Tutorial

---
+++ PushNotifications/Constants.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PushNotifications
+{
+    public static class Constants
+    {
+        public const string SenderID = "AIzaSyD-6WIgLWXBNPc2s-t6_alzRlpgnxjvyU4"; // Google API Project Number
+        public const string ListenConnectionString = "Endpoint=sb://sorensnotificationnamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=LH0ql+BUULTPiG6GXVf0hyu4yx3NDh+GK68+O+gojoM=";
+        public const string NotificationHubName = "sorensnotificationhub";
+    }
+}
---
+++ PushNotifications/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+using Gcm.Client;
+
+namespace PushNotifications
+{
+    [Activity(Label = "PushNotifications", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        public static MainActivity instance; // Add an instance variable to the MainActivity class that will be used to show an alert dialog when the app is running
+
+        protected override void OnCreate(Bundle bundle) // In the OnCreate method of MainActivity.cs, initialize the instance variable and add a call to RegisterWithGCM
+        {
+            instance = this;
+
+            base.OnCreate(bundle);
+
+            // Set your view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get your button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById(Resource.Id.myButton);
+
+            RegisterWithGCM();
+        }
+
+        private void RegisterWithGCM() // Create the following method in the MainActivity class
+        {
+            // Check to ensure everything's set up right
+            GcmClient.CheckDevice(this);
+            GcmClient.CheckManifest(this);
+
+            // Register for push notifications
+            Log.Info("MainActivity", "Registering...");
+            GcmClient.Register(this, Constants.SenderID);
+        }
+    }
+}
+
